d3b9ecb0286ac8a8f65f249d22ff3fe672157a10
arbuztw <arbuz.tw@gmail.com>
2014-11-01 22:06:42 +0800

    done game playing

--- judge.c
+++ judge.c
@@ -5,19 +5,20 @@
 #include <sys/types.h>
 #include <fcntl.h>
 #include <signal.h>
+#define BUFSZ 1024
 #define SWAP(a,b) a^=b^=a^=b
 #define ERR_EXIT(s) { perror(s); exit(1); }
 
 typedef struct {
    int id;
    int num_card;
-   int pipe_fd;
    int pid;
    int key;
+   FILE *pipe;
 } Player;
 
 Player p[4];
-int pile[53];
+int fd_rd, pile[53];
 char child_fifo[4][16];
 
 void init();
@@ -25,11 +26,13 @@ void sort(Player arr[], int n);
 void shuffle(int arr[], int n);
 void make_child_fifo();
 void clean_child_fifo();
+void deal_card();
+int read_from_pipe(int *num);
 
 int main(int argc, char *argv[])
 {
    int i, stat;
-   int fd_rd;
+   int cnt, cur, nxt, num;
    char fname[16], pindex[4], key[8];
 
 
@@ -59,14 +62,52 @@ int main(int argc, char *argv[])
             if (execlp("./player", "player", argv[1], pindex, key, (char*)0) < 0)
                ERR_EXIT("execl");
          }
-         p[i].pipe_fd = open(child_fifo[i], O_WRONLY);
+         p[i].pipe = fopen(child_fifo[i], "w");
       }
 
-      //sleep(5);
+      deal_card();
+
+      cnt = 0;
+      for (i = 0; i < 4; i++)
+         if (p[i].num_card > 0)
+            cnt++;
+
+      cur = 0;
+      while (cnt > 1)
+      {
+         while (p[cur].num_card == 0) cur = (cur + 1) % 4;
+         nxt = (cur + 1) % 4;
+         while (p[nxt].num_card == 0) nxt = (nxt + 1) % 4;
+
+         fprintf(p[cur].pipe, "< %d\n", p[nxt].num_card);
+         fflush(p[cur].pipe);
+
+         read_from_pipe(&num);
+         fprintf(p[nxt].pipe, "> %d\n", num);
+         fflush(p[nxt].pipe);
+         p[nxt].num_card--;
+
+         read_from_pipe(&num);
+         fprintf(p[cur].pipe, "%d\n", num);
+         fflush(p[cur].pipe);
+
+         read_from_pipe(&num);
+         if (num) p[cur].num_card--;
+
+         if (p[cur].num_card == 0) cnt--;
+         if (p[nxt].num_card == 0) cnt--;
+
+         cur = nxt;
+      }
+
       for (i = 0; i < 4; i++) {
          kill(p[i].pid, SIGTERM);
       }
       while (wait(&stat) > 0);
+
+      for (i = 0; i < 4; i++)
+         if (p[i].num_card > 0)
+            printf("%d\n", p[i].id);
    }
 
    clean_child_fifo();
@@ -101,9 +142,9 @@ void init()
    }
    
    pile[0] = 0;
-   for (i = 1; i <= 4; i++)
+   for (i = 0; i < 4; i++)
       for (j = 1; j <= 13; j++)
-         pile[i*j] = j;
+         pile[i*13+j] = j;
    shuffle(pile, 53);
 }
 
@@ -139,4 +180,45 @@ void clean_child_fifo()
       if (unlink(child_fifo[i]) < 0)
          ERR_EXIT("unlink");
    }
+}
+
+void deal_card()
+{
+   int i, j;
+   int pidx, num;
+   for (i = 0; i < 4; i++) {
+      if (i == 0) fprintf(p[i].pipe, "%d ", pile[0]);
+      for (j = 1; j < 13; j++)
+         fprintf(p[i].pipe, "%d ", pile[i*13+j]);
+      fprintf(p[i].pipe, "%d\n", pile[i*13+j]);
+      fflush(p[i].pipe);
+      fprintf(stderr, "deal player %c\n", 'A'+i);
+   }
+
+   for (i = 0; i < 4; i++) {
+      pidx = read_from_pipe(&num);
+      p[pidx].num_card = num;
+   }
+}
+
+int read_from_pipe(int *num)
+{
+   int i, len, pidx, key;
+   char buf[BUFSZ];
+
+   if ((len = read(fd_rd, &buf, BUFSZ)) < 0)
+      ERR_EXIT("read");
+
+   pidx = buf[0] - 'A';
+   key = 0;
+   for (i = 2; buf[i] != ' '; i++)
+      key = key * 10 + buf[i] - '0';
+   if (key != p[pidx].key) {
+      fprintf(stderr, "Someone cheated!\n");
+      exit(1);
+   }
+   *num = 0;
+   for (i++; buf[i]; i++)
+      *num = *num * 10 + buf[i] - '0';
+   return pidx;
 }
