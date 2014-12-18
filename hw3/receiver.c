a64ed95ff426895a16345b65c0a98b714f457f64
arbuztw <arbuz.tw@gmail.com>
2014-12-18 22:05:35 +0800

    open pipe and handle SIGINT

--- hw3/receiver.c
+++ hw3/receiver.c
@@ -1,6 +1,12 @@
 #include <unistd.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
+#define err_exit(s) { perror(s); exit(1); }
+
+FILE *flog;
+
+void sig_int(int signo);
 
 int main(int argc, char *argv[])
 {
@@ -9,10 +15,29 @@ int main(int argc, char *argv[])
       exit(1);
    }
 
-   FILE *flog = fopen("receiver_log", "w");
+   int fd[2];
 
+   pipe(fd);
+   if (fork() == 0) {
+      dup2(fd[1], STDOUT_FILENO);
+      close(fd[0]);
+      close(fd[1]);
+      if (execlp("./sender", "sender", argv[1], (char*)0) < 0)
+         err_exit("execlp");
+   }
+   close(fd[1]);
+
+   flog = fopen("receiver_log", "w");
+
+   signal(SIGINT, sig_int);
 
    fclose(flog);
 
    return 0;
+}
+
+void sig_int(int signo)
+{
+   fprintf(flog, "terminate\n");   
+   exit(0);
 }
